1ef0350e83c166bcf28190dd806eaa3b68ab4d08
kldorseyNU <109479372+kldorseyNU@users.noreply.github.com>
2022-11-16 11:02:07 -0500

    Update Kresling.py
    Fix hinge list bug

--- Kresling.py
+++ Kresling.py
@@ -10,28 +10,31 @@ R -> radius
 P -> edge_length
 L1 -> height
 N -> number_polygon_edges
-phi -> inner_angle
+phi -> half_inner_angle
 '''
 
 # adsk are libraries for Python Fusion API
 import adsk.core, adsk.fusion, adsk.cam
 import math
 
-#Kresling dimensions
-
-radius = 1
-wall_thickness = 0.1 
+#Kresling dimensions 
+# all in cm
+edge_length = 2.5
 height = 2.5
 number_polygon_edges = 6 
 top_rotation_angle = 12 
-ratio_hinge_to_wall = 0.3
+wall_thickness = 0.1 
+
+#Kresling ratios
+hinge_proportion = 0.1
+ratio_hinge_to_wall = 0.5
 ratio_base_to_wall = 0
 
+#Calculate hinge and base thicknesses from ratios
 hinge_thickness = wall_thickness * ratio_hinge_to_wall
 base_thickness = wall_thickness * ratio_base_to_wall
 
-inner_angle = math.pi / number_polygon_edges
-edge_length = radius / math.sin(inner_angle)
+radius = edge_length * (2 * math.sin( math.pi / number_polygon_edges ))
 
 def generate_polygon_points(number_of_kresling_edges, offset_angle, sine_rotation):
     polygon_points = \
@@ -39,6 +42,22 @@ def generate_polygon_points(number_of_kresling_edges, offset_angle, sine_rotatio
         for k in range(number_of_kresling_edges)]
 	
     return polygon_points 
+
+def generate_hinge_points(number_of_kresling_edges, offset_angle, sine_rotation, hinge_proportion):
+    shift_angle = offset_angle + sine_rotation
+    half_inner_angle = math.pi / number_of_kresling_edges
+
+    polygon_points = \
+        [math.cos((2 * k * math.pi / number_of_kresling_edges) - shift_angle ) \
+        for k in range(number_of_kresling_edges)]
+
+    gen_hinge_points = \
+    [(2 * math.sin(half_inner_angle)) * math.sin((2 * item + 1) * half_inner_angle - shift_angle) \
+    for item in range(number_of_kresling_edges)]
+
+    hinge_points_close = [(polygon_points[item] - (hinge_proportion) * gen_hinge_points[item]) for item in range(number_of_kresling_edges)]
+    hinge_points_far = [(polygon_points[item] - (1-hinge_proportion) * gen_hinge_points[item]) for item in range(number_of_kresling_edges)]
+    return hinge_points_close + hinge_points_far
 
 def gen_sketch(points_x, points_y, points_z):
     #Generalized sketch from point list in X, Y, Z
@@ -90,93 +109,92 @@ def param_Kresling(radius, points_x, points_y, points_z):
     Kresling_profile = gen_sketch(points_x_parameterized, points_y_parameterized, points_z)
     return Kresling_profile
 
-def make_Kresling_body(lofts, radius, wall_thickness, hinge_thickness, number_polygon_edges, height, top_rotation_angle, inner_angle, base_thickness):
+def make_Kresling_body(lofts, radius, wall_thickness, hinge_thickness, number_polygon_edges, height, top_rotation_angle, base_thickness,hinge_proportion):
     #Create each Kresling triangle according to specified dimensions
 
     body_list = [] #Make an empty body list to append new bodies to
 
     #Create point lists for Kresling triangle points
-    #nomenclature: point Lower/Upper External/Internal X/Y
     lower_x = generate_polygon_points(number_polygon_edges, 0, 0)
     upper_x = generate_polygon_points(number_polygon_edges, top_rotation_angle, 0)
     lower_y = generate_polygon_points(number_polygon_edges, 0, math.pi/2)
     upper_y = generate_polygon_points(number_polygon_edges, top_rotation_angle, math.pi/2)
 
-    #Create point lists for orthogonal Kresling points to make thinner angles
-    orthogonal_x_neg = generate_polygon_points(number_polygon_edges, inner_angle, 0) 
-    orthogonal_x_pos = generate_polygon_points(number_polygon_edges, -1 * inner_angle, 0)
-    orthogonal_y_neg = generate_polygon_points(number_polygon_edges, inner_angle, math.pi/2)
-    orthogonal_y_pos = generate_polygon_points(number_polygon_edges, -1 * inner_angle, math.pi/2)
-   
-    #Interleave two lists of points
-    orthogonal_x_lower = [val for pair in zip(orthogonal_x_neg, orthogonal_x_pos) for val in pair]
-    orthogonal_y_lower = [val for pair in zip(orthogonal_y_neg, orthogonal_y_pos) for val in pair]
+    lower_hinge_x = generate_hinge_points(number_polygon_edges, 0, 0, hinge_proportion)
+    upper_hinge_x = generate_hinge_points(number_polygon_edges, top_rotation_angle, 0, hinge_proportion)
+    lower_hinge_y = generate_hinge_points(number_polygon_edges, 0, math.pi/2, hinge_proportion)
+    upper_hinge_y = generate_hinge_points(number_polygon_edges, top_rotation_angle, math.pi/2, hinge_proportion)
 
-    orthogonal_x_lower = [i * ((wall_thickness - hinge_thickness) * math.cos(inner_angle)) for i in orthogonal_x_lower]
-    orthogonal_y_lower = [i * ((wall_thickness - hinge_thickness) * math.cos(inner_angle)) for i in orthogonal_y_lower]
-
-    #Create point lists for orthogonal Kresling points to make thinner angles
-    #may have flipped the sign here
-    orthogonal_x_neg = generate_polygon_points(number_polygon_edges, inner_angle + top_rotation_angle , 0) 
-    orthogonal_x_pos = generate_polygon_points(number_polygon_edges, -1 * inner_angle + top_rotation_angle , 0)
-    orthogonal_y_neg = generate_polygon_points(number_polygon_edges, inner_angle + top_rotation_angle , math.pi/2)
-    orthogonal_y_pos = generate_polygon_points(number_polygon_edges, -1 * inner_angle + top_rotation_angle , math.pi/2)
-   
-    #Interleave two lists of points
-    orthogonal_x_upper = [val for pair in zip(orthogonal_x_neg, orthogonal_x_pos) for val in pair]
-    orthogonal_y_upper = [val for pair in zip(orthogonal_y_neg, orthogonal_y_pos) for val in pair]
-
-    #Multiply all points in list by tcos(inner_angle)
-    orthogonal_x_upper = [i * ((wall_thickness - hinge_thickness) * math.cos(inner_angle)) for i in orthogonal_x_upper]
-    orthogonal_y_upper = [i * ((wall_thickness - hinge_thickness) * math.cos(inner_angle)) for i in orthogonal_y_upper]
+    lower_hinge_x_l = generate_hinge_points(number_polygon_edges, 0, 0, 1 - hinge_proportion)
+    upper_hinge_x_l = generate_hinge_points(number_polygon_edges, top_rotation_angle, 0, 1 - hinge_proportion)
+    lower_hinge_y_l = generate_hinge_points(number_polygon_edges, 0, math.pi/2, 1 - hinge_proportion)
+    upper_hinge_y_l = generate_hinge_points(number_polygon_edges, top_rotation_angle, math.pi/2, 1 - hinge_proportion)
 
     #Draw upper and lower Kresling triangles from point lists
     for m in range(2):    
         for k in range(number_polygon_edges):
             if m == 0: #make upper or lower triangle, m == 0 is lower
-                #Make innermost Kresling triangle points
-                points_x = [lower_x[k], upper_x[k], lower_x[(k+1) % number_polygon_edges]]
-                points_y = [lower_y[k], upper_y[k], lower_y[(k+1) % number_polygon_edges]]
-                points_z = [0, height, 0]
 
-                if hinge_thickness < wall_thickness:
-                    offsetX = [orthogonal_x_lower[(2 * k + 1) % (2 * number_polygon_edges)], 0, orthogonal_x_lower[(2 * k + 2) % (2 * number_polygon_edges)]]
-                    offsetY = [orthogonal_y_lower[(2 * k + 1) % (2 * number_polygon_edges)], 0, orthogonal_y_lower[(2 * k + 2) % (2 * number_polygon_edges)]]
+                points_z = [0, height, 0]
+                points_x = [lower_x[k], upper_x[k], lower_x[(k + 1) % number_polygon_edges]]
+                points_y = [lower_y[k], upper_y[k], lower_y[(k + 1) % number_polygon_edges]]
+                
+                points_hinge_x = [lower_hinge_x[(k) % len(lower_hinge_x)], upper_x[(k) % number_polygon_edges], lower_hinge_x[(number_polygon_edges + k) % len(lower_hinge_x)]]
+                points_hinge_y = [lower_hinge_y[(k) % len(lower_hinge_x)], upper_y[(k) % number_polygon_edges], lower_hinge_y[(number_polygon_edges + k) % len(lower_hinge_x)]]
 
             else:
-                points_x = [upper_x[k],lower_x[(k + 1) % number_polygon_edges], upper_x[(k+1) % number_polygon_edges]]
-                points_y = [upper_y[k],lower_y[(k + 1) % number_polygon_edges], upper_y[(k+1) % number_polygon_edges]]
                 points_z = [height, 0, height]
+                points_x = [upper_x[k],lower_x[(k + 1) % number_polygon_edges], upper_x[(k + 1) % number_polygon_edges]]
+                points_y = [upper_y[k],lower_y[(k + 1) % number_polygon_edges], upper_y[(k + 1) % number_polygon_edges]]
 
-                if hinge_thickness < wall_thickness:
-                    offsetX = [orthogonal_x_upper[(2 * k + 1) % (2 * number_polygon_edges)], 0, orthogonal_x_upper[(2 * k + 2) % (2 * number_polygon_edges)]]
-                    offsetY = [orthogonal_y_upper[(2 * k + 1) % (2 * number_polygon_edges)], 0, orthogonal_y_upper[(2 * k + 2) % (2 * number_polygon_edges)]]
+                points_hinge_x = [upper_hinge_x[(k) % len(lower_hinge_x)], lower_x[(k+1) % number_polygon_edges], upper_hinge_x[(number_polygon_edges + k) % len(lower_hinge_x)]]
+                points_hinge_y = [upper_hinge_y[(k) % len(lower_hinge_x)], lower_y[(k+1) % number_polygon_edges], upper_hinge_y[(number_polygon_edges + k) % len(lower_hinge_x)]]
 
             #Make the Kresling triangles
-            outer_kresling= param_Kresling(radius, points_x, points_y, points_z)
-            center_kresling= param_Kresling(radius - hinge_thickness, points_x, points_y, points_z)
-            
-            #Loft between interior and exterior Kresling faces
-            outer_loft = add_loft_feature(lofts,[outer_kresling,center_kresling]) 
+            outer_kresling = param_Kresling(radius, points_x, points_y, points_z)
+            center_kresling = param_Kresling(radius - hinge_thickness, points_x, points_y, points_z)
 
-            #Create bodies from loft features
+            #Loft between interior and exterior Kresling faces, create bodies from loft features
+            outer_loft = add_loft_feature(lofts,[outer_kresling,center_kresling]) 
             outer_bodies = outer_loft.bodies.item(0)
             body_list.append(outer_bodies)
 
-            if hinge_thickness < wall_thickness:
-                points_x_ortho_draw = [(radius-wall_thickness) * points_x[0] + offsetX[0], (radius-hinge_thickness) * points_x[1] + offsetX[1], (radius-wall_thickness) * points_x[2] + offsetX[2]]
-                points_y_ortho_draw = [(radius-wall_thickness) * points_y[0] + offsetY[0], (radius-hinge_thickness) * points_y[1] + offsetY[1], (radius-wall_thickness) * points_y[2] + offsetY[2]]    
-
-                inner_kresling= param_Kresling(radius - wall_thickness, points_x, points_y, points_z)
-                orthogonal_kresling= param_Kresling(1, points_x_ortho_draw, points_y_ortho_draw, points_z)    
-                inner_loft = add_loft_feature(lofts,[orthogonal_kresling,inner_kresling])
-        
-                #Create bodies from loft features
+            
+            if hinge_thickness < wall_thickness: 
+                inner_kresling = param_Kresling(radius - wall_thickness, points_hinge_x, points_hinge_y, points_z)
+                inner_loft = add_loft_feature(lofts,[inner_kresling, center_kresling]) 
                 inner_bodies = inner_loft.bodies.item(0)
                 body_list.append(inner_bodies)
+            
 
     if base_thickness > 0:
         make_base([i * radius for i in lower_x],[i * radius for i in lower_y], base_thickness, lofts, body_list)
+
+    '''
+    #Chambers
+    for m in range(1):    
+        for k in range(number_polygon_edges):
+            if m == 0: #make upper or lower triangle, m == 0 is lower
+                #Make innermost Kresling triangle points
+
+                points_z = [0, height, 0]
+                points_x = [0, upper_x[k], lower_x[(k) % number_polygon_edges]]
+                points_y = [0, upper_y[k], lower_y[(k) % number_polygon_edges]]
+
+            else:
+                points_z = [height, 0, height]
+                points_x = [0,lower_x[(k + 1) % number_polygon_edges], upper_x[(k) % number_polygon_edges]]
+                points_y = [0,lower_y[(k + 1) % number_polygon_edges], upper_y[(k) % number_polygon_edges]]
+
+            #Make the Kresling triangles
+            origin_kresling = param_Kresling(radius, points_x, points_y, points_z)
+            #center_kresling = param_Kresling(radius - hinge_thickness, points_x, points_y, points_z)
+
+            #Loft between interior and exterior Kresling faces, create bodies from loft features
+            #outer_loft = add_loft_feature(lofts,[outer_kresling,center_kresling]) 
+            #outer_bodies = outer_loft.bodies.item(0)
+            #body_list.append(outer_bodies)
+        '''
 
     return body_list
 
@@ -194,8 +212,5 @@ sketchObjs = rootComp.sketches
 cPlaneObjs = rootComp.constructionPlanes
 loftFeats = rootComp.features.loftFeatures
 
-
-
-
-##Make Kresling structure
-Kresling = make_Kresling_body(loftFeats, edge_length , wall_thickness, hinge_thickness, number_polygon_edges, height , top_rotation_angle * (math.pi/180), inner_angle, base_thickness)
+# Make Kresling structure
+Kresling = make_Kresling_body(loftFeats, edge_length, wall_thickness, hinge_thickness, number_polygon_edges, height , top_rotation_angle * (math.pi/180), base_thickness,hinge_proportion)
